45ac175f48ee4092423b693600932f3b59ddd6da
marouanekrizi <kaprim41@outlook.com>
2023-10-18 13:41:21 +0100

    Add Home to the  project

--- NZWalksUI/Controllers/HomeController.cs
+++ NZWalksUI/Controllers/HomeController.cs
@@ -1,21 +1,40 @@
 ﻿using System.Diagnostics;
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 using NZWalksUI.Models;
+using NZWalksUI.Models.DTO;
 
 namespace NZWalksUI.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IHttpClientFactory _httpClientFactory;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
     {
         _logger = logger;
+        _httpClientFactory = httpClientFactory;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        List<WalksDTO> response = new();
+        try
+        {
+            //Get All Wlaks from Web Api
+            var client = _httpClientFactory.CreateClient();
+            var httpResponseMessage = await client.GetAsync("https://localhost:7094/api/walks");
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var stringResponse = await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<WalksDTO>>();
+            response.AddRange(stringResponse);
+        }
+        catch (Exception ex)
+        {
+            //Log the exception
+        }
+        return View(response);
     }
 
     public IActionResult Privacy()
